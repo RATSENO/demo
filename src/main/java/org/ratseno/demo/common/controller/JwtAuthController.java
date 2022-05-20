dfce397f3d758a7c29abe94a725f199476c3ba19
RATSENO <onestar1014@gmail.com>
2022-05-20 15:23:02 +0900

    토큰 발급 API 추가

---
+++ src/main/java/org/ratseno/demo/common/controller/JwtAuthController.java
@@ -0,0 +1,58 @@
+package org.ratseno.demo.common.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.ratseno.demo.common.security.domain.CustomUser;
+import org.ratseno.demo.common.security.jwt.provider.JwtTokenProvider;
+import org.ratseno.demo.domain.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping({"/auth"})
+public class JwtAuthController {
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthController.class);
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthController(JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager){
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @PostMapping({"/token"})
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody Member member){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(member.getUserName(), member.getUserPw());
+        Authentication authentication = this.authenticationManager.authenticate((Authentication) usernamePasswordAuthenticationToken);
+        
+        CustomUser user = (CustomUser) authentication.getPrincipal();
+        long userNo = user.getUserNo();
+        String userId = user.getUserId();
+        List<String> roles = (List<String>) user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        String token = "Bearer " + this.jwtTokenProvider.createToken(userNo, userId, roles);
+        Map<String, String> res = new HashMap<>();
+        res.put("token", token);
+        return new ResponseEntity(res, HttpStatus.OK);
+    }
+}
