b5d018039dc12259a7f5de6dfff9cb3e222530b3
RATSENO <onestar1014@gmail.com>
2022-05-22 12:24:26 +0900

    @requestbody import 수정

--- src/main/java/org/ratseno/demo/common/controller/JwtAuthController.java
+++ src/main/java/org/ratseno/demo/common/controller/JwtAuthController.java
@@ -18,11 +18,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping({"/auth"})
