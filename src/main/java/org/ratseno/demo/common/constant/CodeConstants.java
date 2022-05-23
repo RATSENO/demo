1d22716aef4b8e0d9d83e514637ef4ae1539fccb
RATSENO <onestar1014@gmail.com>
2022-05-23 17:06:13 +0900

    상수 코드 추가

--- src/main/java/org/ratseno/demo/common/constant/CodeConstants.java
+++ src/main/java/org/ratseno/demo/common/constant/CodeConstants.java
@@ -2,4 +2,8 @@ package org.ratseno.demo.common.constant;
 
 public class CodeConstants {
     public final static String SUCCESS = "2000";
+
+    /** 9000번대 비지니스 로직 에러코드 */
+    public final static String REQUIRED_PARAMETER_ERROR = "9001";
+    public final static String JSON_STR_PARSE_ERROR = "9002";
 }
