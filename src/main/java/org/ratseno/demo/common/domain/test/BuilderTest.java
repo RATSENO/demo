c6ef2eebe751f73add8cf7d8afcb6c505fe31e8d
RATSENO <onestar1014@gmail.com>
2022-06-14 16:41:33 +0900

    builder

--- src/main/java/org/ratseno/demo/common/domain/test/BuilderTest.java
+++ src/main/java/org/ratseno/demo/common/domain/test/BuilderTest.java
@@ -1,8 +1,17 @@
 package org.ratseno.demo.common.domain.test;
 
+import lombok.Builder;
+
 public class BuilderTest {
     private String name;
     private String adderss;
     private String phoneNumber;
     private String sex;
+    @Builder
+    public BuilderTest(String name, String adderss, String phoneNumber, String sex){
+        this.name = name;
+        this.adderss = adderss;
+        this.phoneNumber = phoneNumber;
+        this.sex = sex;
+    }
 }
